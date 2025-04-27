1623120898ae5e1391be9e052255da27de5f83ca
ravisoujiaitraining <ncnallan4@gmail.com>
2025-04-28 01:54:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ app.config.from_object(Config)
 
 db.init_app(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 @app.route('/')
 def home():
     return render_template('home.html')
