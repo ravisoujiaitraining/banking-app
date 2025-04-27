a75759c4063eacf56ce764ebcbbb97cc501e7238
ravisoujiaitraining <ncnallan4@gmail.com>
2025-04-28 01:28:07 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    balance = db.Column(db.Float, default=0.0)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('account.id'))
+    receiver_id = db.Column(db.Integer, db.ForeignKey('account.id'))
+    amount = db.Column(db.Float, nullable=False)
